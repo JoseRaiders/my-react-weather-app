db08695fdef56bb5abea218399bf42f2542a71ba
JoseRaiders <71295774+JoseRaiders@users.noreply.github.com>
2021-11-12 18:25:57 -0800

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-      <a href="https://github.com/JoseRaiders/my-react-weather-app" target="_blank" rel="noreferrer">Open source on GitHub</a>{" "}
-      coded by <a href="https://jovial-ardinghelli-beabc4.netlify.app/about.html" target="_blank" rel="noreferrer">Anuska Jose</a>.
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+        <a href="https://github.com/JoseRaiders/my-react-weather-app" target="_blank" rel="noreferrer">Open source on GitHub</a>{" "}
+        coded by <a href="https://jovial-ardinghelli-beabc4.netlify.app/about.html" target="_blank" rel="noreferrer">Anuska Jose</a>.
+        </footer>
+      </div>
     </div>
   );
 }
